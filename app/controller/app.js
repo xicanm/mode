b0fa85ab76c5c885f5bf4a8f118db248d111c555
Ovidiu Chereches <skidding@gmail.com>
2012-07-21 13:13:57 +0300

    Added notifications and arranged app controller

--- app/controller/app.js
+++ app/controller/app.js
@@ -2,11 +2,38 @@ var mode = require('mode');
 
 mode.controller.App = mode.controller.Base.extend(function()
 {
-	this.init = function(callback)
+	this.init = function(cb)
 	{
+		/*
+			Store session reference
+		*/
+		this.session = this.conn.req.session;
+		/*
+			Init and store notification reference
+		*/ 
+		this.notifications = mode.notification.init(this.session);
+		/*
+			Pass notification to view vars
+		*/
+		this.args.notifications = this.notifications;
+		/*
+			Set default view layout
+		*/
 		this.args.layout = 'default';
-
-		callback.respond();
+		/*
+			Set default page title
+		*/
+		this.args.title = 'node.js project';
+		/*
+			End action asynchronously, even though no
+			aync action is taking place here, to make
+			it extendable by children with async init
+		*/
+		cb.respond();
+	};
+	this.notify = function(type, message)
+	{
+		this.notifications.add(type, message);
 	};
 },
 true);
--- app/controller/main.js
+++ app/controller/main.js
@@ -4,6 +4,6 @@ mode.controller.Main = require('./app.js').extend(function()
 {
 	this.index = function()
 	{
-		this.args.title = 'node.js project';
+				
 	};
 });
